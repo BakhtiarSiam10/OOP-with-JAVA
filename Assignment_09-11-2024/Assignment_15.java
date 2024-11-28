c210e7b10321a1a6be69db7e1f44b2faaedb7832
BakhtiarSiam10 <siam.islam7426@gmail.com>
2024-11-28 22:01:33 +0600

    Assignment_15.java done

--- Assignment_09-11-2024/Assignment_15.java
+++ Assignment_09-11-2024/Assignment_15.java
@@ -1,1 +1,18 @@
+//15. Write a Java program that displays the sum of n odd natural numbers.
+import java.util.Scanner;
 
+public class Assignment_15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += 2 * i - 1;
+        }
+
+        System.out.println("Sum: " + sum);
+        scanner.close();
+    }
+}
