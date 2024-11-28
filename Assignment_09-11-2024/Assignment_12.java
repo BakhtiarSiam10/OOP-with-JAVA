c12abc80481aa19f4f3860a5935398a9915f4b31
BakhtiarSiam10 <siam.islam7426@gmail.com>
2024-11-28 21:34:01 +0600

    Assignment_12.java done

--- Assignment_09-11-2024/Assignment_12.java
+++ Assignment_09-11-2024/Assignment_12.java
@@ -1,1 +1,23 @@
+//12.	Write a program in Java to input 5 numbers from the keyboard and find their sum and average.
+import java.util.Scanner;
 
+public class Assignment_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 5;
+
+        for (int i = 1; i <= count; i++) {
+        System.out.print("Enter number " + i + ": ");
+        sum += scanner.nextInt();
+        }
+
+       double average = sum / (double) count;
+
+       System.out.println("Sum: " + sum);
+       System.out.println("Average: " + average);
+
+       scanner.close();
+    }
+}
