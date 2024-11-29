a0c7c0e58236dd3ce8e1d50d15cc24ac27da7ff0
BakhtiarSiam10 <siam.islam7426@gmail.com>
2024-11-29 20:27:21 +0600

    Assignment_18.java done

---
+++ Assignment_09-11-2024/Right Angle Triangle Increasing Numbers.java
@@ -0,0 +1,20 @@
+//18. Write a Java program to make such a pattern like a right-angle triangle with the number increased by 1.
+import java.util.Scanner;
+
+public class RightAngleTriangleIncreasingNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+        
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
